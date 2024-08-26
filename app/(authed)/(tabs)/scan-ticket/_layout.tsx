22b1e18f4098151204ca922758ea2bc2deaa93f2
Satyam <satyamjha1607@gmail.com>
2024-08-27 01:16:16 +0530

    feat: Add QR code scanning functionality to ticket scanning page

--- app/(authed)/(tabs)/scan-ticket/_layout.tsx
+++ app/(authed)/(tabs)/scan-ticket/_layout.tsx
@@ -1,10 +1,58 @@
-import { Text, View } from "react-native";
-export default function ScanLayout(){
-  
+import { ActivityIndicator, Alert, Text, Vibration, View } from "react-native";
+import { BarcodeScanningResult, CameraView, useCameraPermissions } from "expo-camera";
+import { VStack } from "@/components/VStack";
+import Button from "@/components/Button";
+import { useState } from "react";
+import { ticketService } from "@/services/tickets";
+export default function ScanLayout() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scanningEnabled, setScanningEnabled] = useState(true);
+
+  if (!permission) {
+    return <VStack flex={1} justifyContent='center' alignItems='center'>
+      <ActivityIndicator size={"large"} />
+    </VStack>;
+  }
+
+  if (!permission.granted) {
+    return (
+      <VStack gap={20} flex={1} justifyContent='center' alignItems='center'>
+        <Text>Camera access is required to scan tickets.</Text>
+        <Button onPress={requestPermission}>Allow Camera Access</Button>
+      </VStack>
+    );
+  }
+  async function onBarcodeScanned({ data }: BarcodeScanningResult) {
+    if (!scanningEnabled) return;
+
+    try {
+      Vibration.vibrate();
+      setScanningEnabled(false);
+
+      const [ticket, owner] = data.split(",");
+      const ticketId = parseInt(ticket.split(":")[1]);
+      const ownerId = parseInt(owner.split(":")[1]);
+
+      await ticketService.validateOne(ticketId, ownerId);
+
+      Alert.alert('Success', "Ticket validated successfully.", [
+        { text: 'Ok', onPress: () => setScanningEnabled(true) },
+      ]);
+
+    } catch (error) {
+      Alert.alert('Error', "Failed to validate ticket. Please try again.");
+      setScanningEnabled(true);
+    }
+  }
 
   return (
-    <View>
-      <Text>Scan Layout</Text>
-    </View>
+    <CameraView
+      style={{ flex: 1 }}
+      facing="back"
+      onBarcodeScanned={onBarcodeScanned}
+      barcodeScannerSettings={{
+        barcodeTypes: ["qr"],
+      }}
+    />
   );
 };
